32d3536c295f6db168be8fcc0c0ba964dd589b93
Douglas Man <douglas.man@me.com>
2016-08-12 11:08:58 -0400

    ..wsdl ..

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,10 +1,9 @@
 package com.demo;
 
 
-import gov.va.mdws.emrsvc.EmrSvcSoap;
-import gov.va.mdws.emrsvc.HospitalLocationTO;
-import gov.va.mdws.emrsvc.TaggedVisitArray;
-import gov.va.mdws.emrsvc.VisitTO;
+//import gov.va.mdws.emrsvc.*;
+import gov.va.medora.mdws.emrsvc.*;
+//import net.webservicex.StockQuoteSoap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -20,36 +19,37 @@ public class App {
     public static void main( String[] args )
     {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"client-beans.xml"});
-        EmrSvcSoap port = (EmrSvcSoap)context.getBean("emrService");
+        EmrSvcSoap port = (EmrSvcSoap)context.getBean("emrSvc");
 
-//        OrderProcess port = (OrderProcess)context.getBean("orderClient");
 
-//        OrderProcessImplService orderProcess = new OrderProcessImplService();
-//        OrderProcess port = orderProcess.getOrderProcessImplPort();
+        RegionArray regionArray = port.getVHA();
 
-//        Order order = new Order();
-//        order.setCustomerID("C001");
-//        order.setItemID("I001");
-//        order.setPrice(100.00);
-//        order.setQty(20);
-//        String result = port.processOrder(order);
+        for (RegionTO regionTO: regionArray.getRegions().getRegionTO()) {
+            LOGGER.debug("Name: "+regionTO.getName()+", ID: "+regionTO.getId());
+        }
+
+
+//        String quote = port.getQuote("AAPL");
+//        DataSourceArray dataSourceArray = port.connect("688");
+        LOGGER.debug("port: "+ port);
+
 //        LOGGER.debug("port: "+ port);
 
-        String clinicId;
-        String clinicName;
-        String facilityName;
-        String status;
-        HospitalLocationTO location;
-
-        TaggedVisitArray vArray = port.getVisits("20150808.000000", "20160808.235959");
-        for (VisitTO visitTO : vArray.getVisits().getVisitTO()) {
-            status = visitTO.getStatus();
-            location = visitTO.getLocation();
-            clinicName = location.getName();
-
-            LOGGER.debug("visit status: {}%", status);
-            LOGGER.debug("location name: {}%", clinicName);
-        }
+//        String clinicId;
+//        String clinicName;
+//        String facilityName;
+//        String status;
+//        HospitalLocationTO location;
+//
+//        TaggedVisitArray vArray = port.getVisits("20150808.000000", "20160808.235959");
+//        for (VisitTO visitTO : vArray.getVisits().getVisitTO()) {
+//            status = visitTO.getStatus();
+//            location = visitTO.getLocation();
+//            clinicName = location.getName();
+//
+//            LOGGER.debug("visit status: {}%", status);
+//            LOGGER.debug("location name: {}%", clinicName);
+//        }
         // Iterator<VisitTO> iter = vArray.getVisitTO()
 
 //        LOGGER.debug("getVisits result: "+ port.getVisits("20150808.000000", "20160808.235959"));
